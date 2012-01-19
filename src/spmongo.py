13ff869cf40a3d645aefc57a86849ea2404ef4b7
Matt Bornski <matt@bornski.com>
2012-01-19 12:50:21 -0800

    Collect stats

--- src/spmongo.py
+++ src/spmongo.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-__version_info__ = (0, 2, 9)
+__version_info__ = (0, 3, 0)
 __version__ = '.'.join([str(i) for i in __version_info__])
 version = __version__
 
+import collections
 import pymongo
 import pymongo.errors
 import random
@@ -13,7 +14,6 @@ import time
 import traceback
 
 try:
-    # Are you using gevent?
     import gevent.hub
     import gevent.queue
     import gevent.greenlet
@@ -24,7 +24,8 @@ try:
         def __init__(self, *args, **kwargs):
             self.internal_timeout = 0.1
             self.network_timeout = 3.0
-            self.pool_size = 10
+            self.pool_size = int(kwargs['max_pool_size'])
+            del kwargs['max_pool_size']
             self._lock = gevent.coros.RLock()
             self._count = 0
             self._used = {}
@@ -85,9 +86,12 @@ MONGO_DOWN_NICE = 0.5
 
 class _wrapped_object(object):
     __reconnection_wrapper_in_effect = False
-    def __init__(self, obj, **kwargs):
+    def __init__(self, obj, reporter, **kwargs):
         self.__class__ = type(obj.__class__.__name__, (self.__class__, obj.__class__), {})
         self.__dict__ = obj.__dict__
+        self._reporter = reporter
+    def _report(self, *args, **kwargs):
+        self._reporter._report(*args, **kwargs)
     def _reconnect(self, fn, *args, **kwargs):
         if not self.__reconnection_wrapper_in_effect:
             warning_printed = False
@@ -116,7 +120,7 @@ class _wrapped_cursor(_wrapped_object, pymongo.cursor.Cursor):
 
 class _wrapped_collection(_wrapped_object, pymongo.collection.Collection):
     distinct = lambda self, *args, **kwargs: self._reconnect(pymongo.collection.Collection.distinct, *args, **kwargs)
-    find = lambda self, *args, **kwargs: _wrapped_cursor(self._reconnect(pymongo.collection.Collection.find, *args, **kwargs), slave_okay=self._slave_okay)
+    find = lambda self, *args, **kwargs: _wrapped_cursor(self._reconnect(pymongo.collection.Collection.find, *args, **kwargs), self._reporter)
     map_reduce = lambda self, *args, **kwargs: self._reconnect(pymongo.collection.Collection.map_reduce, *args, **kwargs)
     update = lambda self, *args, **kwargs: self._reconnect(pymongo.collection.Collection.update, *args, **kwargs)
     insert = lambda self, *args, **kwargs: self._reconnect(pymongo.collection.Collection.insert, *args, **kwargs)
@@ -144,21 +148,28 @@ class _wrapped_collection(_wrapped_object, pymongo.collection.Collection):
     def find_one(self, *args, **kwargs):
         # We must retry on primary if this find_one query went to a secondary and found no results.
         try:
-            ret = self._reconnect(pymongo.collection.Collection.find_one, *args, **kwargs)
+            start = time.time()
+            ret = self._reconnect(pymongo.collection.Collection.find_one, *args, read_preference=pymongo.ReadPreference.SECONDARY, **kwargs)
+            self._report('find_one', 'secondary', time.time() - start)
             assert(ret is not None)
         except AssertionError:
+            start = time.time()
             ret = self._reconnect(pymongo.collection.Collection.find_one, *args, read_preference=pymongo.ReadPreference.PRIMARY, **kwargs)
+            self._report('find_one', 'primary', time.time() - start)
         return ret
 
 class _wrapped_database(_wrapped_object, pymongo.database.Database):
     def __getattr__(self, *args, **kwargs):
         ret = pymongo.database.Database.__getattr__(self, *args, **kwargs)
         if isinstance(ret, pymongo.collection.Collection):
-            return _wrapped_collection(ret)
+            return _wrapped_collection(ret, self._reporter)
         else:
             return ret
 
 class mongo(object):
+    _totals = {
+        'operations':collections.defaultdict(lambda: collections.defaultdict(lambda: {'count':0, 'times':{'total':0, 'max':0}}))
+    }
     def __init__(self, *args, **kwargs):
         self._connection = None
         self._hosts = []
@@ -174,12 +185,10 @@ class mongo(object):
         self._rsname = kwargs.get('replicaset')
         try:
             assert(self._rsname is not None)
-            self._pool = self._make_pool()
+            self._pool = _gevent_safe_connection_pool(','.join(self._hosts), max_pool_size=kwargs.get('max_pool_size', 10), replicaSet=self._rsname, read_preference=pymongo.ReadPreference.SECONDARY)
         except (AssertionError, NameError):
             self._pool = None
-    def _make_pool(self):
-        return _gevent_safe_connection_pool(','.join(self._hosts), max_pool_size=1, replicaSet=self._rsname, read_preference=pymongo.ReadPreference.SECONDARY)
-    def connection(self):
+    def _get_connection(self):
         if self._pool is not None:
             # Pull from pool.  This should return the one already in use by this greenlet if any.
             return self._pool.get()
@@ -200,10 +209,16 @@ class mongo(object):
             self._connection.end_request()
             self._connection = None
     def database(self, name):
-        return _wrapped_database(self.connection()[name])
+        return _wrapped_database(self._get_connection()[name], self)
     def collection(self, db, collection):
         return self.database(db).__getitem__(collection)
     def __getattr__(self, key):
         return self.database(key)
     def __getitem__(self, key):
         return self.database(key)
+    def _report(self, operation, host, duration):
+        self._totals['operations'][operation][host]['count'] += 1
+        self._totals['operations'][operation][host]['times']['total'] += duration
+        self._totals['operations'][operation][host]['times']['max'] = max(self._totals['operations'][operation][host]['times']['max'], duration)
+    def statistics(self):
+        return self._totals
